2efef6f078e9bb188f1751698226686bfebff963
= <=>
2013-07-01 10:17:06 +0200

    solved publicurl notvalid method

--- evernote/lib/public_url.rb
+++ evernote/lib/public_url.rb
@@ -6,7 +6,7 @@ class PublicUrl
 	attr_accessor :user_name, :notebook_name, :host
 
 	def initialize(publicUrl)
-		raise BadArgumentException, 'empty.url' if notValid
+		raise BadArgumentException, 'empty.url' if notValid publicUrl
         @user_name = Extractor.extractUsername(publicUrl)
         @notebook_name = Extractor.extractNotebookName(publicUrl)
         @host = Extractor.extractHost(publicUrl)
@@ -14,7 +14,7 @@ class PublicUrl
 
 	private 
 
-		def notValid
+		def notValid(publicUrl)
 			publicUrl.nil? || publicUrl.empty?
 		end
 	
