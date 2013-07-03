f47d747876b81fc8ced99dc097d90c97ca7d0121
= <=>
2013-07-03 16:06:24 +0200

    renaming app.js to skip js deploy and serverup processing

---
+++ web/public/js/spin-opts.js
@@ -0,0 +1,8 @@
+var APP = {};
+APP.spinOpts = {
+  lines: 12, // The number of lines to draw
+  length: 10, // The length of each line
+  width: 6, // The line thickness
+  radius: 12,
+  color: 'orange'
+ };
